fcf671abed11ddec5534972480ed21fa29d719d7
codxr02 <56270568+codxr02@users.noreply.github.com>
2020-09-22 15:58:39 +0200

    Stunning animations

--- mario-fake/Assets/Scripts/MarioMovement.cs
+++ mario-fake/Assets/Scripts/MarioMovement.cs
@@ -5,13 +5,27 @@ using UnityEngine;
 public class MarioMovement : MonoBehaviour {
 	
 	[SerializeField] private float moveSpeed = default;
+	
+	private Animator animator;
+	
+	private void Start() {
+		animator = GetComponent<Animator>();
+	}
 
     void Update() {
         if (Input.GetKey(KeyCode.A)) {
 			transform.position = new Vector2(transform.position.x - (1f * moveSpeed * Time.deltaTime), 0f);
+			animator.SetBool("IsWalkingLeft", true);
+			animator.SetBool("IsWalkingRight", false);
 		}
 		else if (Input.GetKey(KeyCode.D)) {
 			transform.position = new Vector2(transform.position.x + (1f * moveSpeed * Time.deltaTime), 0f);
+			animator.SetBool("IsWalkingLeft", false);
+			animator.SetBool("IsWalkingRight", true);
+		}
+		else {
+			animator.SetBool("IsWalkingRight", false);
+			animator.SetBool("IsWalkingLeft", false);
 		}
     }
 }
