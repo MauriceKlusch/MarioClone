fa1dd0896743bc8b4e8abe79fb16d586cfaa8614
Maurice Klusch <maurice.klusch@gmx.de>
2020-09-23 09:12:53 +0200

    new map sprite

--- mario-fake/Assets/Scripts/MarioMovement.cs
+++ mario-fake/Assets/Scripts/MarioMovement.cs
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class MarioMovement : MonoBehaviour {
-	
-	[SerializeField] private float moveSpeed = default;
-	
-	private Animator animator;
-	
-	private void Start() {
-		animator = GetComponent<Animator>();
-	}
-
-    void Update() {
-        if (Input.GetKey(KeyCode.A)) {
-			transform.position = new Vector2(transform.position.x - (1f * moveSpeed * Time.deltaTime), 0f);
-			animator.SetBool("IsWalkingLeft", true);
-			animator.SetBool("IsWalkingRight", false);
-		}
-		else if (Input.GetKey(KeyCode.D)) {
-			transform.position = new Vector2(transform.position.x + (1f * moveSpeed * Time.deltaTime), 0f);
-			animator.SetBool("IsWalkingLeft", false);
-			animator.SetBool("IsWalkingRight", true);
-		}
-		else {
-			animator.SetBool("IsWalkingRight", false);
-			animator.SetBool("IsWalkingLeft", false);
-		}
-    }
-}
-=======
 ﻿using UnityEngine;
 
 namespace mariofake {
@@ -155,4 +122,3 @@ namespace mariofake {
 	}
 
 }
->>>>>>> feature/movement
