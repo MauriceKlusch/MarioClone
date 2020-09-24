8d2b6beba2c30b44d4685334ab234444fdf08c2e
Maurice Klusch <maurice.klusch@gmx.de>
2020-09-24 22:40:58 +0200

    Fixed pause menu not working sometimes

--- mario-fake/Assets/Scripts/PauseMenu.cs
+++ mario-fake/Assets/Scripts/PauseMenu.cs
@@ -10,10 +10,6 @@ namespace mariofake {
 
         public static bool isPaused = false;
 
-        private void Start() {
-            pauseMenu.SetActive(false);
-        }
-
         // Update is called once per frame
         void Update() {
             if (Input.GetKeyDown(KeyCode.Escape))
