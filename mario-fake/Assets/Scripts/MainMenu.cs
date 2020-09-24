6e29f6622a3c8904e543e5b40ecb05e630d92a73
Moritz Gailing <Moritz.Gailing@de.bosch.com>
2020-09-24 08:56:55 +0200

    Menu is feddisch

--- mario-fake/Assets/Scripts/MainMenu.cs
+++ mario-fake/Assets/Scripts/MainMenu.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject MainaMenu = default;
+    [SerializeField] private GameObject LevelaSelect = default;
+
+
     public void PlayGame()
     {
-        SceneManager.LoadScene(1);
+        LevelaSelect.SetActive(true);
+        MainaMenu.SetActive(false);
     }
     public void QuitGame()
     {
