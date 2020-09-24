a028e02ffaba1a84f39b3fa445e87e5caf4e71f3
Maurice Klusch <maurice.klusch@gmx.de>
2020-09-24 11:07:44 +0200

    Added level timer

---
+++ mario-fake/Assets/Scripts/LevelTimer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace mariofake {
+    public class LevelTimer : MonoBehaviour {
+        [SerializeField] private float levelTime = 120f;
+        [SerializeField] private Text timeText;
+
+        // Start is called before the first frame update
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+            levelTime -= Time.deltaTime;
+            string minutes = ((int)levelTime / 60).ToString("00");
+            string seconds = ((int)(levelTime % 60)).ToString("00");
+            timeText.text = minutes + ":" + seconds;
+
+            if (levelTime < 0) {
+                // todo: die
+            }
+        }
+    }
+}
