7eae73569a2d2cf3bd0061d1017326d824d322e4
Moritz Gailing <Moritz.Gailing@de.bosch.com>
2020-09-24 13:21:23 +0200

    ya

---
+++ mario-fake/Assets/Scripts/DeathScreen.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+namespace mariofake { 
+public class DeathScreen : MonoBehaviour
+{
+    
+        int currentscene = Convert.ToInt32(SceneManager.GetActiveScene());
+       public void Respawn()
+        {
+            SceneManager.LoadScene(currentscene);
+        }
+        public void TitleScreen()
+        {
+            SceneManager.LoadScene(0);
+        }
+            
+    }
+}
