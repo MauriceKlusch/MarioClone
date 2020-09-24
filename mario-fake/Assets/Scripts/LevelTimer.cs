17b8d15eb404656bb436510894b7680af803b0b4
Maurice Klusch <maurice.klusch@gmx.de>
2020-09-24 12:15:46 +0200

    Timer fix when Mario is dead

--- mario-fake/Assets/Scripts/LevelTimer.cs
+++ mario-fake/Assets/Scripts/LevelTimer.cs
@@ -16,13 +16,16 @@ namespace mariofake {
 
         // Update is called once per frame
         void Update() {
-            levelTime -= Time.deltaTime;
-            string minutes = ((int)levelTime / 60).ToString("00");
-            string seconds = ((int)(levelTime % 60)).ToString("00");
-            timeText.text = minutes + ":" + seconds;
-
             if (levelTime < 0) {
-                marioHealth.KillMario();
+                timeText.text = "00:00";
+
+                if (marioHealth != null)
+                    marioHealth.KillMario();
+            } else {
+                levelTime -= Time.deltaTime;
+                string minutes = ((int)levelTime / 60).ToString("00");
+                string seconds = ((int)(levelTime % 60)).ToString("00");
+                timeText.text = minutes + ":" + seconds;
             }
         }
     }
