2359bd330546173cf7c865a8e693d0af1355232f
codxr02 <56270568+codxr02@users.noreply.github.com>
2020-09-24 16:10:59 +0200

    Added cloud spawner

---
+++ mario-fake/Assets/Scripts/CloudSpawner.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace mariofake {
+
+    public class CloudSpawner : MonoBehaviour {
+
+        [Header("Cloud")]
+        [SerializeField] private Cloud cloud = default;
+        [Header("Settings")]
+        [SerializeField] private Transform minYTransform = default;
+        [SerializeField] private Transform maxYTransform = default;
+        [SerializeField] private float minSpawningInterval = default;
+        [SerializeField] private float maxSpawningInterval = default;
+
+        private float yPos;
+        private float timer;
+
+        private void Start() {
+            yPos = Random.Range(minYTransform.position.y, maxYTransform.position.y);
+            timer = Random.Range(minSpawningInterval, maxSpawningInterval);
+        }
+
+        private void Update() {
+            if (timer > 0) {
+
+                timer -= Time.deltaTime;
+                if (timer <= 0) {
+                    Instantiate(cloud, new Vector3(transform.position.x, yPos, 0f), Quaternion.identity);
+
+                    yPos = Random.Range(minYTransform.position.y, maxYTransform.position.y);
+                    timer = Random.Range(minSpawningInterval, maxSpawningInterval);
+                }
+            } 
+        }
+
+    }
+
+
+}
